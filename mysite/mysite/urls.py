2e0e64ad163ef439d541c7c75aff33a80bb673f9
Kalu <kaluawa5555@gmail.com>
2021-05-19 16:27:28 -0500

    second commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#
+# ]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^portfolio/', include('portfolio.urls')),
+    url(r'^', include('portfolio.urls')),
 ]
